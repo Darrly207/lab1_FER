9611286aca123a882cc03fc6ece2b195eb7f1b17
Darrly207 <147229115+Darrly207@users.noreply.github.com>
2025-01-07 11:15:57 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h2>Wellcome to my Lab</h2>
+      <p>My first React app</p>
+    </div>
+  );
+}
+
+export default App;
